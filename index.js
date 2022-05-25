caf1e7ea2a96e88e6755064ceabd40b58b1d08b6
hiba <hibzrashed@gmail.com>
2022-05-25 16:50:08 -0500

    commit tree d930209570777b6e478553e2cd7b86ca86238147

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then ((resp) => resp.json())
+    .then((books) => renderBooks(books));
 }
+fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
